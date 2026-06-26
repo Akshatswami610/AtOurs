38c469de17c6de7b10f0a51ac6b5c2d1886b2b2a
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-06-26 10:33:13 +0530

    Update urls.py

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #frontend pages
+    path('', home, name='home'),
+    path('home', home, name='home'),
+
 ]
