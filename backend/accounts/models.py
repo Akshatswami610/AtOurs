eb637423247bf8967c3adb6f4f2589670829482e
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-06-30 10:38:58 +0530

    Update models.py

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, phone_number, name, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        if not phone_number:
+            raise ValueError("Phone number is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email,phone_number=phone_number,name=name,**extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, phone_number, name, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        return self.create_user(
+            email,
+            phone_number,
+            name,
+            password,
+            **extra_fields
+        )
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=150)
+    email = models.EmailField(unique=True,db_index=True)
+    phone_number = models.CharField(max_length=15,unique=True,db_index=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["phone_number", "name"]
+
+    def __str__(self):
+        return self.name
