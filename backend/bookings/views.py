8422c98b4c7ce01de6e2d9e84cea83802546a630
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-07-08 14:27:29 +0530

    Update views.py

--- backend/bookings/views.py
+++ backend/bookings/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Booking
+from .serializers import BookingSerializer
+
+
+class BookingListCreateView(generics.ListCreateAPIView):
+
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Booking.objects.filter(
+            user=self.request.user
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class BookingDetailView(generics.RetrieveAPIView):
+
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Booking.objects.filter(
+            user=self.request.user
+        )
