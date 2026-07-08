540250e1ab6173a00bec018514867cf3f3f0b3ee
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-07-08 14:27:01 +0530

    Update admin.py

--- backend/bookings/admin.py
+++ backend/bookings/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = ( "id", "user", "event", "quantity", "payment_status", "booking_status", "booked_at",)
+    list_filter = ( "payment_status", "booking_status",)
+    search_fields = ( "full_name", "email", "phone_number", "razorpay_order_id",)
+
+    ordering = ("-booked_at",)
