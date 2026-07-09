d28604a979b4afcaeb5e891de95ce16078823161
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-07-09 22:19:07 +0530

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, MeView
+from .views import RegisterView, LoginView, LogoutView, MeView, ForgotPasswordView, VerifyOTPView, ResetPasswordView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('me/', MeView.as_view(), name='me'),
+    path( "forgot-password/", ForgotPasswordView.as_view(), name="forgot-password" ),
+    path( "verify-otp/", VerifyOTPView.as_view(), name="verify-otp" ),
+    path( "reset-password/", ResetPasswordView.as_view(), name="reset-password" ),
 ]
