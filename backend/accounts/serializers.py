ac1858de2a09c7e5da7bfc632ee1e41bf4480c06
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-07-10 12:29:58 +0530

    Update serializers.py

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -141,3 +141,20 @@ class ResetPasswordSerializer(serializers.Serializer):
             })
 
         return attrs
+
+# ==========================================================
+# CHANGE PASSWORD
+# ==========================================================
+
+class ChangePasswordSerializer(serializers.Serializer):
+    current_password = serializers.CharField( write_only=True)
+    new_password = serializers.CharField( write_only=True, min_length=8)
+    confirm_password = serializers.CharField( write_only=True)
+
+    def validate(self, attrs):
+        if attrs["new_password"] != attrs["confirm_password"]:
+            raise serializers.ValidationError({
+                "confirm_password":
+                    "Passwords do not match."
+            })
+        return attrs
