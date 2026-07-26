e32110b9711f2d85575086e39627253a9dfd9068
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-07-26 17:28:22 +0530

    Update urls.py

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -2,16 +2,22 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("me/",MeView.as_view()),
-    path("users/<int:user_id>/",PublicUserProfileView.as_view()),
-    path("register/send-otp/",RegisterSendOTPView.as_view()),
-    path("register/verify/",RegisterVerifyOTPView.as_view()),
-    path("login/",LoginView.as_view()),
-    path("login/send-otp/",LoginSendOTPView.as_view()),
-    path("login/verify/",LoginVerifyOTPView.as_view()),
-    path("forgot-password/",ForgotPasswordView.as_view()),
-    path("verify-otp/",VerifyOTPView.as_view()),
-    path("reset-password/",ResetPasswordView.as_view()),
-    path("change-password/",ChangePasswordView.as_view(),name="change-password"),
-    path("logout/",LogoutView.as_view()),
+    path("me/", MeView.as_view()),
+    path("users/<int:user_id>/", PublicUserProfileView.as_view()),
+    path("interests/", InterestListView.as_view()),
+
+    path("register/send-otp/", RegisterSendOTPView.as_view()),
+    path("register/verify/", RegisterVerifyOTPView.as_view()),
+
+    path("login/", LoginView.as_view()),
+    path("login/send-otp/", LoginSendOTPView.as_view()),
+    path("login/verify/", LoginVerifyOTPView.as_view()),
+
+    path("forgot-password/", ForgotPasswordView.as_view()),
+    path("verify-otp/", VerifyOTPView.as_view()),
+    path("reset-password/", ResetPasswordView.as_view()),
+
+    path("change-password/", ChangePasswordView.as_view(), name="change-password"),
+
+    path("logout/", LogoutView.as_view()),
 ]
