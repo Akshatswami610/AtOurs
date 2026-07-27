3108215fecbdb5d6e9cb22f5ba6b8c9fce945982
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-07-27 14:33:34 +0530

    Update models.py

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -115,6 +115,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField( unique=True, db_index=True,)
     phone_number = models.CharField( max_length=15, unique=True, db_index=True, validators=[phone_validator],)
     date_of_birth = models.DateField()
+    is_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True,)
     is_staff = models.BooleanField(default=False,)
     date_joined = models.DateTimeField(auto_now_add=True,)
