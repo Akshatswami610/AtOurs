bd29acf580b2fef0dc219a984f02cc313917a63d
Akshat Swami <143736434+Akshatswami610@users.noreply.github.com>
2026-08-28 15:04:36 +0530

    Created saved events urls

--- backend/hosting/urls.py
+++ backend/hosting/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import EventListCreateView, EventDetailView
+from .views import EventListCreateView, EventDetailView, SaveEventView, RemoveSavedEventView, SavedEventListView
 
 urlpatterns = [
     path("events/", EventListCreateView.as_view(), name="event-list"),
     path("events/<int:event_id>/", EventDetailView.as_view(), name="event-detail"),
+    path("events/<int:event_id>/save/", SaveEventView.as_view(), name="save-event"),
+    path("events/<int:event_id>/unsave/", RemoveSavedEventView.as_view(), name="remove-saved-event"),
+    path("saved-events/", SavedEventListView.as_view(), name="saved-events"),
 ]
